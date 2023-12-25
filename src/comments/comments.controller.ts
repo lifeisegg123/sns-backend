04d42bbc4f7e2df8063527677e4e1413fe347258
lifeisegg123 <leejj2002@naver.com>
2023-12-25 22:10:16 +0900

    chore: comment swagger

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -2,13 +2,18 @@ import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CommentEntity } from './entities';
 
+@ApiTags('comments')
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
   @UseGuards(JwtAuthGuard)
   @Post()
+  @ApiResponse({ status: 200, type: CommentEntity })
+  @ApiBody({ type: CreateCommentDto })
   create(@Body() createCommentDto: CreateCommentDto, @Request() req) {
     return this.commentsService.create(createCommentDto, req.user.userId);
   }
--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsUUID } from 'class-validator';
 
 export class CreateCommentDto {
+  @ApiProperty()
   @IsString()
   desc: string;
 
+  @ApiProperty()
   @IsUUID()
   postId: string;
 }
